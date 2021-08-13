e1f14e5ea94d9573ba12634a34b694bbfd3647ab
Sashoq <36050942+Sashoq@users.noreply.github.com>
2021-08-13 16:28:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+let numberOfDrumbuttons = document.querySelectorAll(".drum").length;
+for (i = 0; i < numberOfDrumbuttons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+
+    switch (buttonInnerHTML) {
+      case "w":
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case "a":
+        let kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+
+      case "s":
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+      case "d":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      case "j":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+      case "k":
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+      case "l":
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      default: console.log(buttonInnerHTML);
+
+    }
+  });
+}
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      let kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    case "s":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default: console.log(key);
+}
+};
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
